4314c33b0e87f4724b7dd787803e0271e257ac77
Vira Lytvyn <Vira_Lytvyn@epam.com>
2014-09-16 10:05:11 +0300

    Implemented saving files to special place at server side. #18

--- js/formatsConverter.js
+++ js/formatsConverter.js
@@ -2,10 +2,12 @@ function exportFromJSON () {
 	var path = $(this).val();
 	var filemane = path.indexOf('\\') ? path.split('\\').pop() : path.split('/').pop() ;
 	console.log(filemane);
-	// var file = this.files[0];
+	filemane = 'test';
+	console.log(filemane);
+	var file = this.files[0];
 
-	// var blob = new Blob([file], {type: "application/json"});
-	// saveAs(blob, "/user_quizes/" + filemane);
+	var blob = new Blob([file], {type: "application/json"});
+	saveAs(blob, "/user_quizes/" + filemane);
 
 	$.getJSON("./user_quizes/"+filemane, function(json) {
 	    console.log(json); 
